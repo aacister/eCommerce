cc5b97390d7a1ace85fb6ce4a0159b918cc8269d
Andrew Cisternino <aacister@gmail.com>
2024-12-03 16:29:54 -0500

    Add project files.

---
+++ eCommerce.Core/DTO/LoginRequest.cs
@@ -0,0 +1,9 @@
+﻿
+
+namespace eCommerce.Core.DTO
+{
+    public record LoginRequest(
+        string? Email,
+        string? Password);
+    
+}
---
+++ ecommerce.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿
+using eCommerce.Core.RepositroyContracts;
+using eCommerce.Infrastructure.DbContext;
+using eCommerce.Infrastructure.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace eCommerce.Infrastructure
+{
+    public static class DependencyInjection
+    {
+
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        {
+            //TODO: Add services to the IoC container
+            services.AddTransient<IUsersRepository, UsersRepository>();
+            services.AddTransient<DapperDbContext>();
+            return services;
+        }
+    }
+}
